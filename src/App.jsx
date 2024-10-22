f098aee65b8b1b8efeae99cb01e1de2b3e19d109
tubi55 <tubi55@nate.com>
2024-10-22 14:26:38 +0900

    기본 페이지 컴포넌트 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Contact from './pages/Contact';
@@ -5,16 +6,22 @@ import Gallery from './pages/Gallery';
 import Member from './pages/Member';
 import Post from './pages/Post';
 import Youtube from './pages/Youtube';
+import Home from './pages/Home';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Member />
-			<Gallery />
-			<Youtube />
-			<Contact />
-			<Post />
+
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/member' element={<Member />} />
+				<Route path='/gallery' element={<Gallery />} />
+				<Route path='/youtube' element={<Youtube />} />
+				<Route path='/contact' element={<Contact />} />
+				<Route path='/post' element={<Post />} />
+			</Routes>
+
 			<Footer />
 		</>
 	);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className='home'>
+			<h2>HOME</h2>
+		</main>
+	);
+}
