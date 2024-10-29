3dee23f1334170fde6051622eab03278433c4585
tubi55 <tubi55@nate.com>
2024-10-29 20:35:18 +0900

    검색기능 추가

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom';
 
 export default function Post() {
 	const [Posts, setPosts] = useState([]);
+	//검색어가 담길 state
+	const [SearchText, setSearchText] = useState('');
+
+	//검색폼에서 submit 이벤트 발생시 input요소의 입력값으로 SearchText상태 변경
+	const handleSubmit = e => {
+		e.preventDefault();
+		setSearchText(e.target[0].value);
+	};
 
 	useEffect(() => {
 		axios.get('http://localhost:8000/posts').then(res => {
@@ -13,12 +21,30 @@ export default function Post() {
 		});
 	}, []);
 
+	// SearchText 상태값 변경시마다 해당 상태값으로 GET요청 처리
+	useEffect(() => {
+		if (!SearchText) return;
+		axios
+			.get(`http://localhost:8000/posts-search/?search=${SearchText}`)
+			.then(res => {
+				console.log(res.data);
+				setPosts(res.data);
+			})
+			.catch(err => console.log(err.message));
+	}, [SearchText]);
+
 	return (
 		<Layout title='Post'>
+			<form className='searchBox' onSubmit={handleSubmit}>
+				<input type='text' />
+				<button>Search</button>
+			</form>
+
 			<button>
 				<Link to='/post-add'>Write Post</Link>
 			</button>
-			{Posts.map(post => {
+
+			{Posts?.map(post => {
 				return (
 					<h3 key={post.id}>
 						<Link to={`/post/${post.slug}`}>{post.title}</Link>
