de84ec4ba2d3e9590e3818c720ab42ed32493d4c
tubi55 <tubi55@nate.com>
2024-10-29 14:47:23 +0900

    상세페이지에서 수정버튼 클릭시 수정컴포넌트의 폼요소에 get방식으로 수정할 데이터 출력

--- src/pages/PostEdit.jsx
+++ src/pages/PostEdit.jsx
@@ -1,5 +1,37 @@
+import { useParams } from 'react-router-dom';
 import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function PostEdit() {
-	return <Layout title='Edit Post'>PostEdit</Layout>;
+	const { slug } = useParams();
+	const [Data, setData] = useState(null);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:8000/posts/${slug}`)
+			.then(res => {
+				setData(res.data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<Layout title='Edit Post'>
+			<form onSubmit={handleSubmit}>
+				<input type='text' value={Data?.title} />
+				<br />
+				<textarea value={Data?.body}></textarea>
+				<br />
+			</form>
+			<input type='reset' value='수정취소' />
+			<input type='submit' value='수정' />
+		</Layout>
+	);
 }
