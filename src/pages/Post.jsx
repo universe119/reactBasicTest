a3aabf912f5ecbc869ae6f8117628be95277554e
tubi55 <tubi55@nate.com>
2024-10-29 11:48:21 +0900

    Post컴포넌트에서 GET방식으로 서버 데이터 호출

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,27 @@
 import Layout from '../components/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Post() {
+	const [Posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		axios.get('http://localhost:8000/posts').then(res => {
+			console.log(res.data);
+			setPosts(res.data);
+		});
+	}, []);
+
 	return (
 		<Layout title='Post'>
-			<p>Post Page</p>
+			{Posts.map(post => {
+				return (
+					<h3 key={post.id}>
+						<Link to={`/detail/${post.slug}`}>{post.title}</Link>
+					</h3>
+				);
+			})}
 		</Layout>
 	);
 }
