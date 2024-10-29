17b6dbf31030ee2e52e5ed0dbcad42bb73e7fe9e
tubi55 <tubi55@nate.com>
2024-10-29 21:44:35 +0900

    카테고리 선택 메뉴기능

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -5,23 +5,38 @@ import { Link } from 'react-router-dom';
 
 export default function Post() {
 	const [Posts, setPosts] = useState([]);
-	//검색어가 담길 state
 	const [SearchText, setSearchText] = useState('');
+	//카테고리명이 담길 state
+	const [Category, setCategory] = useState('');
 
-	//검색폼에서 submit 이벤트 발생시 input요소의 입력값으로 SearchText상태 변경
+	//전체 포스트 목록 초기화를 편하게 하도록 함수 분리
+	const fetchAllPosts = () => {
+		axios.get('http://localhost:8000/posts').then(res => {
+			console.log(res.data);
+			setPosts(res.data);
+		});
+	};
+
 	const handleSubmit = e => {
 		e.preventDefault();
 		setSearchText(e.target[0].value);
 	};
 
+	// Category 상태값에 따라 기존 Posts에서 해당 카테고리명에 매칭되는 데이터만 filtering해서 FilteredPosts라는 이름의 state로 저장
+	// 삼항연사자의 구조를 응용해서 마치 if, else if, else 구문처럼 활용 가능
+	const FilteredPosts =
+		Category === 'BUSINESS'
+			? Posts.filter(post => post.category == 'BUSINESS')
+			: Category === 'PERSONAL'
+			? Posts.filter(post => post.category == 'PERSONAL')
+			: Category === 'IMPORTANT'
+			? Posts.filter(post => post.category == 'IMPORTANT')
+			: Posts;
+
 	useEffect(() => {
-		axios.get('http://localhost:8000/posts').then(res => {
-			console.log(res.data);
-			setPosts(res.data);
-		});
+		fetchAllPosts();
 	}, []);
 
-	// SearchText 상태값 변경시마다 해당 상태값으로 GET요청 처리
 	useEffect(() => {
 		if (!SearchText) return;
 		axios
@@ -40,17 +55,23 @@ export default function Post() {
 				<button>Search</button>
 			</form>
 
+			<select onChange={e => setCategory(e.target.value)}>
+				<option value=''>All Notes</option>
+				<option value='BUSINESS'>Business</option>
+				<option value='PERSONAL'>Personal</option>
+				<option value='IMPORTANT'>Important</option>
+			</select>
+
 			<button>
 				<Link to='/post-add'>Write Post</Link>
 			</button>
 
-			{Posts?.map(post => {
-				return (
-					<h3 key={post.id}>
-						<Link to={`/post/${post.slug}`}>{post.title}</Link>
-					</h3>
-				);
-			})}
+			{/* FilteredPosts 상태값으로 반복 렌더링 */}
+			{FilteredPosts?.map(post => (
+				<h3 key={post.id}>
+					<Link to={`/post/${post.slug}`}>{post.title}</Link>
+				</h3>
+			))}
 		</Layout>
 	);
 }
