69ad7e77218b2ac977b110d631e7abca9f17386b
universe119 <ruddh119@gmail.com>
2024-10-31 13:23:30 +0900

    각 박스 세로위치값으로 초기화된 스크롤박 내부 컨텐츠 스타일에 연동

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,15 @@
-export default function News({ Scroll }) {
+export default function News({ Scroll, pos }) {
+	const currentScroll = Scroll - pos || 0;
+
+	const style_h2 = {
+		left: currentScroll * 6
+	};
+	const style_h3 = { transform: `translateX(${currentScroll * 1.5}px) ` };
+
 	return (
 		<section className='news'>
-			<h2>POST PREVIEW</h2>
-			<h3>INFORMATION</h3>
+			<h2 style={style_h2}>POST</h2>
+			<h3 style={style_h3}>INFORMATION</h3>
 
 			<article></article>
 			<article></article>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,41 @@
 import Visual from '../components/Visual';
 import News from '../components/News';
 import Pics from '../components/Pics';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
 	const [Scroll, setScroll] = useState(0);
-	console.log(Scroll);
+	const ref_wrap = useRef(null);
+	const ref_posArr = useRef([]);
 
 	// 스크롤 이벤트 핸들러 함수
 	const handleScroll = () => {
 		setScroll(window.scrollY); // 현재 스크롤 위치를 상태에 저장
 	};
 
+	const handleResize = () => {
+		ref_posArr.current = [];
+		for (const el of ref_wrap.current.children) {
+			ref_posArr.current.push(el.offsetTop);
+		}
+		// console.log(ref_posArr.current);
+	};
+
 	// 컴포넌트 마운트 시 이벤트 추가 및 언마운트 시 제거
 	useEffect(() => {
 		window.addEventListener('scroll', handleScroll); // 스크롤 이벤트 연결
+		window.addEventListener('resize', handleResize);
 		return () => {
 			window.removeEventListener('scroll', handleScroll); // 언마운트 시 스크롤 이벤트 제거
+			window.removeEventListener('resize', handleResize);
 		};
 	}, []);
 
 	return (
-		<>
+		<div ref={ref_wrap}>
 			<Visual Scroll={Scroll}></Visual>
-			<News Scroll={Scroll}></News>
+			<News Scroll={Scroll} pos={ref_posArr.current[1]}></News>
 			<Pics Scroll={Scroll}></Pics>
-		</>
+		</div>
 	);
 }
