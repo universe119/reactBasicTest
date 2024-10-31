1e252ea1c93b1177cef60ec25894cd619eac1d63
universe119 <ruddh119@gmail.com>
2024-10-31 11:06:54 +0900

    미션 1,2 와 스크롤 모션scale, opacity등

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,7 +1,20 @@
 export default function Visual({ Scroll }) {
+	// 미션2 - 제목요소가 확대되면서 완전 투명해지도록 스크롤값 연동 (scale과 opacity로)
+
+	// 강사님 주석 - 아래와 같이 스크롤 값을 연동하면 Scroll초기값이 0이 기존 scss스타일을 덮어쓰기 하기 때문에 초기 위치가 틀어짐
+
+	// 미션1 - 현재 위치에서 스크롤이 되면 오른쪽으로 움직이도록 처리 (transform사용)
+
+	// 내 주석 - 기존 스타일에 있는 값 보다(left랑 top이있는 상태인데)스크립트가 우선순위라서 여기 style_h2가 먼저 먹음
+
+	const style_h2 = {
+		transform: `translateX(${Scroll}px) translateY(${Scroll}px) rotate(${Scroll * 2}deg) scale(${1 + Scroll / 400})`,
+		opacity: `${1 - Scroll / 400}`
+	};
+
 	return (
 		<div className='visual'>
-			<h2>ALPIACO {Scroll}</h2>
+			<h2 style={style_h2}>ALPACO</h2>
 		</div>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,7 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
 	const [Scroll, setScroll] = useState(0);
-
-	// 윈도우 스크롤시 현재 스크롤되는 거리값을 Scroll상태값에 담아주는 함수 생성
-	// 해당 함수를 윈도우 스크롤이벤트 연결
-	// 컴포넌트 언마운트시 스크롤이벤트 해체
-	// 비주얼 컴포넌트에 스크롤값 전달
-	// Visual안쪽에서 전달되는 Scroll값을 h2요소 원하는 스타일 구문에 연동
+	console.log(Scroll);
 
 	// 스크롤 이벤트 핸들러 함수
 	const handleScroll = () => {
